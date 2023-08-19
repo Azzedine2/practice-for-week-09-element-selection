5d37ea366634cc748165c5f9aad2ca3ec3e0f032
Azzedine2 <136700190+Azzedine2@users.noreply.github.com>
2023-08-19 10:26:19 +0200

    commit

--- selectors.js
+++ selectors.js
@@ -1,45 +1,50 @@
 const select = () => {
-    /* Write queries for each of the following */
+  /* Write queries for each of the following */
 
-    /* Section 1 */
-    // 1. Get all seeded fruit elements
-    // Your code here
-    const seeded = document.getElementsByClassName("seed")
-    console.log(`seed: ${seeded}`)
-    
-    // 2. Get all seedless fruit elements
-    // Your code here
-    const seedless = document.getElementsByClassName("seedless")
-    console.log(`seedless: ${seedless}`)
+  /* Section 1 */
+  // 1. Get all seeded fruit elements
+  // Your code here
+  const seeded = document.getElementsByClassName("seed");
+  console.log(`seed: ${seeded}`);
 
-    // 3. Get first seedless fruit element
-    // Your code here
-    console.log(seedless[0])
+  // 2. Get all seedless fruit elements
+  // Your code here
+  const seedless = document.getElementsByClassName("seedless");
+  console.log(`seedless: ${seedless}`);
 
-    /* Section 2 */
-    // 4. Get inner span with text "you"
-    // Your code here
-    const span = document.querySelector("span")
-    console.log(`Span adress: ${span}`)
+  // 3. Get first seedless fruit element
+  // Your code here
+  console.log(seedless[0]);
 
-    // 5. Get all children of element "wrapper"
-    // Your code here
+  /* Section 2 */
+  // 4. Get inner span with text "you"
+  // Your code here
+  const span = document.querySelector("span");
+  console.log(`Span adress: ${span}`);
 
-    // 6. Get all odd number list items in the list
-    // Your code here
+  // 5. Get all children of element "wrapper"
+  // Your code here
+  const childs = document.querySelector("#wrapper").children;
 
-    // 7. Get all even number list items in the list
-    // Your code here
+  console.log(`Children: ${childs}`);
 
-    /* Section 3 */
-    // 8. Get all tech companies without a class name
-    // Your code here
+  // 6. Get all list items in the list`)
 
-    // 9. Get "Amazon" list element
-    // Your code here
+  // 6. Get all odd number list items in the list
+  // Your code here
 
-    // 10. Get all unicorn list elements (not the image element)
-    // Your code here
-}
+  // 7. Get all even number list items in the list
+  // Your code here
+
+  /* Section 3 */
+  // 8. Get all tech companies without a class name
+  // Your code here
+
+  // 9. Get "Amazon" list element
+  // Your code here
+
+  // 10. Get all unicorn list elements (not the image element)
+  // Your code here
+};
 
 window.onload = select;
