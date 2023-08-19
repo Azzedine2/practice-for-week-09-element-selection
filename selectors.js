b4c46d6e0abdcdd4bd97628b4f425fbecf8fc26b
Azzedine2 <136700190+Azzedine2@users.noreply.github.com>
2023-08-19 11:02:00 +0200

    new

--- selectors.js
+++ selectors.js
@@ -4,12 +4,12 @@ const select = () => {
   /* Section 1 */
   // 1. Get all seeded fruit elements
   // Your code here
-  const seed = document.querySelectorAll("seed");
+  const seed = document.querySelectorAll(".seed");
   console.log(`seed: ${seed}`);
 
   // 2. Get all seedless fruit elements
   // Your code here
-  const seedless = document.querySelectorAll("seedless");
+  const seedless = document.querySelectorAll(".seedless");
   console.log(`seedless: ${seedless}`);
 
   // 3. Get first seedless fruit element
@@ -34,20 +34,24 @@ const select = () => {
 
   // 6. Get all odd number list items in the list
   // Your code here
-  const odd = document.querySelectorAll("odd");
+  const odd = document.querySelectorAll(".odd");
   console.log(`Odd: ${odd}`);
 
   // 7. Get all even number list items in the list
   // Your code here
   const even = document.querySelectorAll("ol li:nth-child(even");
-  console.dir(`Even: ${even}`);
+  console.log(`Even: ${even}`);
 
   /* Section 3 */
   // 8. Get all tech companies without a class name
   // Your code here
+  const liWithoutClass = document.querySelectorAll("#three ul li:not([class])");
+  console.log(`Without Class: ${liWithoutClass}`);
 
   // 9. Get "Amazon" list element
   // Your code here
+  const amazon = document.querySelector(".shopping");
+  console.log(`Amazon: ${amazon}`);
 
   // 10. Get all unicorn list elements (not the image element)
   // Your code here
